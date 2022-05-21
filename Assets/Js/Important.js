0e67b82d3845aff8147845daa175a1dc5dff6b34
DmitrijsSemjonovsDP13 <DmitroUzumaki@yandex.ru>
2022-05-21 22:29:56 +0300

    Fast commit, new laptop

---
+++ Assets/Js/Important.js
@@ -0,0 +1,55 @@
+// <!DOCTYPE html>
+// <html lang="en">
+//     <head>
+//     <meta charset="UTF-8">
+//     <link rel="stylesheet" href="Assets/css/MyDayStyle.css">
+//     <title>My Day</title>
+// </head>
+// <body>
+// <header class="header">
+//     <div class="container">
+//         <div class="header_inner">
+//             <div class="Logo">My Day</div>
+//             <nav class="NavigationBar">
+//                 <a class="Nav_link" href="index.html">Back</a>
+//                 <form id="New-task-form">
+//                     <input
+//                         type="text"
+//                         id="New-task-input"
+//                         placeholder="What do you have planned ? " />
+//                     <input
+//                         type="submit"
+//                         id="New-task-submit"
+//                         value="Add Task" />
+//                 </form>
+//
+//             </nav>
+//         </div>
+//     </div>
+// </header>
+//
+// <main>
+//     <section class="Task-list">
+//         <div id="tasks">
+//             <div class="task">
+//                 <div class="content">
+//                     <input
+//                         type="text"
+//                         class="text"
+//                         value="My shiny task"
+//                         readonly
+//                     />
+//                 </div>
+//                 <div class="acton">
+//                     <button class="Edit">Edit</button>
+//                     <button class="Delete">Delete</button>
+//                 </div>
+//             </div>
+//
+//
+//         </div>
+//     </section>
+// </main>
+// <script src="Assets/Js/MyDay.js"
+// </body>
+// </html>
